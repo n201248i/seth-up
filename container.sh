a9c01b9ba52c657b8394b8ca03141c194c30e2ae
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2025-03-22 23:55:37 +0000

    chore: experiments

---
+++ container.sh
@@ -0,0 +1,29 @@
+if ! command -v bun &> /dev/null; then
+    echo "bun is not installed"
+    curl -fsSL https://bun.sh/install | bash
+fi
+
+if ! command -v node &> /dev/null; then
+    echo "node is not installed"
+    exit 1
+fi
+
+if ! command -v npm &> /dev/null; then
+    echo "npm is not installed"
+    exit 1
+fi
+
+if ! command -v git &> /dev/null; then
+    echo "git is not installed"
+    exit 1
+fi
+
+if ! grep -q "^#n201248i$" .gitignore; then
+    printf "\n\n#n201248i\n" >> .gitignore
+    printf "node_modules\n" >> .gitignore
+    printf "bun.lock\n" >> .gitignore
+    printf "*.dev\n" >> .gitignore
+    printf "*.data\n" >> .gitignore
+fi
+
+bun init -y
